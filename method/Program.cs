d1a78389e086693a9c40d1a350e42e8db5b03c4e
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:42:58 +0600

    first commit

---
+++ method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Math 
+    {
+         public int add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            int s = math.add(10,20);
+            Console.WriteLine(s);
+            
+           
+        }
+    }
+}
